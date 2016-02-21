1e07d7af3501528b0c4d99a047c871d10e16f995
alexcrist <alexecrist@gmail.com>
2016-02-21 16:38:49 -0500

    Updated README

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A basic skeleton for MEAN stack projects.
 
 One controller, one router, no MongoDB stuff yet.
+
+To run, navigate to `app` folder and run `node serverApp.js`.
--- app/routes/router.js
+++ app/routes/router.js
@@ -3,9 +3,9 @@
 var express = require('express'),
     controller = require('../controllers/mainController');
 
-    var router = express.Router();
+var router = express.Router();
 
-    router.route('/')
+router.route('/')
     .get(function(req, res, next) {
         res.sendFile(path.join(__dirname + '../public/index.html'));
     });
